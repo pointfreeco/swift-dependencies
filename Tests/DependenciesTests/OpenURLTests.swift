07d3bcc0183a42dc2d90733d2c881b3d088f1dd0
Zeeshan Mian <zmian@me.com>
2023-03-04 22:14:30 -0500

    linting

--- Tests/DependenciesTests/OpenURLTests.swift
+++ Tests/DependenciesTests/OpenURLTests.swift
@@ -4,45 +4,44 @@ import XCTestDynamicOverlay
 
 final class OpenURLTests: XCTestCase {
   func testOpenUrl() async {
-      let urlToOpenIsolated = ActorIsolated<URL?>(nil)
+    let urlToOpenIsolated = ActorIsolated<URL?>(nil)
 
-      let viewModel = withDependencies {
-          $0.openURL = .init { url in
-              await urlToOpenIsolated.setValue(url)
-              return true
-          }
-      } operation: {
-          ViewModel()
+    let viewModel = withDependencies {
+      $0.openURL = .init { url in
+        await urlToOpenIsolated.setValue(url)
+        return true
       }
+    } operation: {
+      ViewModel()
+    }
 
-      var urlToOpen = await urlToOpenIsolated.value
-      XCTAssertNil(urlToOpen)
+    var urlToOpen = await urlToOpenIsolated.value
+    XCTAssertNil(urlToOpen)
 
-      await viewModel.openMailApp()
-      urlToOpen = await urlToOpenIsolated.value
-      XCTAssertEqual(urlToOpen, .mailApp)
+    await viewModel.openMailApp()
+    urlToOpen = await urlToOpenIsolated.value
+    XCTAssertEqual(urlToOpen, .mailApp)
 
-      await viewModel.openSomeUrl()
-      urlToOpen = await urlToOpenIsolated.value
-      XCTAssertEqual(urlToOpen, URL(string: "https://example.com"))
+    await viewModel.openSomeUrl()
+    urlToOpen = await urlToOpenIsolated.value
+    XCTAssertEqual(urlToOpen, URL(string: "https://example.com"))
   }
 }
 
 private final class ViewModel {
-    @Dependency(\.openURL) var openURL
+  @Dependency(\.openURL) var openURL
 
-    func openMailApp() async {
-        await openURL(.mailApp)
-    }
+  func openMailApp() async {
+    await openURL(.mailApp)
+  }
 
-    func openSomeUrl() async {
-        await openURL(URL(string: "https://example.com")!)
-    }
+  func openSomeUrl() async {
+    await openURL(URL(string: "https://example.com")!)
+  }
 }
 
-
 extension URL {
-    fileprivate static var mailApp: Self {
-        URL(string: "message://")!
-    }
+  fileprivate static var mailApp: Self {
+    URL(string: "message://")!
+  }
 }

8dd8f48ce099f98c57aac9d2145c232bb96b3e40
Brandon Williams <mbrandonw@hey.com>
2023-03-06 09:35:11 -0800

    Fire-and-forget should not participate in cooperative cancellation.

--- Tests/DependenciesTests/FireAndForgetTests.swift
+++ Tests/DependenciesTests/FireAndForgetTests.swift
@@ -1,6 +1,7 @@
 import Dependencies
 import XCTest
 
+@MainActor
 final class FireAndForgetTests: XCTestCase {
   @Dependency(\.fireAndForget) var fireAndForget
 
@@ -11,6 +12,23 @@ final class FireAndForgetTests: XCTestCase {
       try await Task.sleep(nanoseconds: 100_000_000)
       await didExecute.setValue(true)
     }
+
+    let value = await didExecute.value
+    XCTAssertEqual(value, true)
+  }
+
+  func testTestContext_Cancellation() async throws {
+    let didExecute = ActorIsolated(false)
+
+    let task = Task {
+      await self.fireAndForget {
+        try await Task.sleep(nanoseconds: 1_000_000_000)
+        await didExecute.setValue(true)
+      }
+    }
+    try await Task.sleep(nanoseconds: 500_000_000)
+    task.cancel()
+    await task.value
 
     let value = await didExecute.value
     XCTAssertEqual(value, true)
@@ -36,21 +54,23 @@ final class FireAndForgetTests: XCTestCase {
     }
   }
 
-  func testLiveContext_DependencyAccess() async {
-    await withDependencies {
-      $0.context = .live
-      $0.date.now = Date(timeIntervalSince1970: 1_234_567_890)
-    } operation: {
-      let date = ActorIsolated<Date?>(nil)
+  #if !os(Linux)
+    func testLiveContext_DependencyAccess() async {
+      await withDependencies {
+        $0.context = .live
+        $0.date.now = Date(timeIntervalSince1970: 1_234_567_890)
+      } operation: {
+        let date = ActorIsolated<Date?>(nil)
 
-      await self.fireAndForget {
-        @Dependency(\.date.now) var now: Date
-        await date.setValue(now)
+        await self.fireAndForget {
+          @Dependency(\.date.now) var now: Date
+          await date.setValue(now)
+        }
+
+        while await date.value == nil {}
+        let value = await date.value
+        XCTAssertEqual(value, Date(timeIntervalSince1970: 1_234_567_890))
       }
-
-      while await date.value == nil {}
-      let value = await date.value
-      XCTAssertEqual(value, Date(timeIntervalSince1970: 1_234_567_890))
     }
-  }
+  #endif
 }

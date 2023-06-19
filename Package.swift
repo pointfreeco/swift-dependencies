39bcd4d0d87a2ae1c609aa8f5a385fa5bbb8e7a7
Stephen Celis <stephen@stephencelis.com>
2023-06-19 15:10:00 -0700

    wip

--- Package.swift
+++ Package.swift
@@ -18,16 +18,20 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
-    .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "0.10.0"),
-    .package(url: "https://github.com/pointfreeco/swift-clocks", from: "0.3.0"),
+//    .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "0.10.0"),
+    .package(path: "../combine-schedulers"),
+    .package(path: "../swift-concurrency-extras"),
+    .package(path: "../swift-clocks"),
+//    .package(url: "https://github.com/pointfreeco/swift-clocks", from: "0.3.0"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.8.0"),
   ],
   targets: [
     .target(
       name: "Dependencies",
       dependencies: [
-        .product(name: "CombineSchedulers", package: "combine-schedulers"),
         .product(name: "Clocks", package: "swift-clocks"),
+        .product(name: "CombineSchedulers", package: "combine-schedulers"),
+        .product(name: "ConcurrencyExtras", package: "swift-concurrency-extras"),
         .product(name: "XCTestDynamicOverlay", package: "xctest-dynamic-overlay"),
       ]
     ),

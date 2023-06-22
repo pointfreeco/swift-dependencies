d0a114e3923b1e5f81f6767ae17ce50d2317701d
Brandon Williams <mbrandonw@hey.com>
2023-06-22 10:49:35 -0400

    Added a test for invokeTest behavior that we may want to change in the future.

---
+++ Tests/DependenciesTests/InvokeTestTests.swift
@@ -0,0 +1,30 @@
+import Dependencies
+import XCTest
+
+@MainActor
+final class InvokeTestTests: XCTestCase {
+  override func invokeTest() {
+    withDependencies {
+      $0.date = .constant(Date(timeIntervalSince1970: 1))
+    } operation: {
+      super.invokeTest()
+    }
+  }
+
+  func testOverride() {
+    class Model {
+      @Dependency(\.date.now) var now
+      func getDate() -> Date { self.now }
+    }
+
+    let model = withDependencies {
+      $0.date = .constant(Date(timeIntervalSince1970: 2))
+    } operation: {
+      Model()
+    }
+
+    XCTExpectFailure {
+      XCTAssertEqual(model.getDate().timeIntervalSince1970, 2)
+    }
+  }
+}

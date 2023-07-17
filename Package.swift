361ec5068b912616a3895d700a4bf515152e95d8
Stephen Celis <stephen@stephencelis.com>
2023-07-17 12:31:30 -0700

    wip

--- Package.swift
+++ Package.swift
@@ -49,7 +49,7 @@ let package = Package(
   ]
 )
 
-#if swift(>=5.6)
+#if !os(Windows)
   // Add the documentation compiler plugin if possible
   package.dependencies.append(
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")

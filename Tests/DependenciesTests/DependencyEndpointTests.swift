f660c88d7d29a4088980bc48e3f76339a2d58445
Stephen Celis <stephen@stephencelis.com>
2023-11-13 11:30:21 -0800

    wip

--- Tests/DependenciesTests/DependencyEndpointTests.swift
+++ Tests/DependenciesTests/DependencyEndpointTests.swift
@@ -1,20 +1,22 @@
-import Dependencies
-import DependenciesMacros
-import XCTest
+#if canImport(DependenciesMacros)
+  import Dependencies
+  import DependenciesMacros
+  import XCTest
 
-final class DependencyEndpointTests: XCTestCase {
-  func testUnimplemented() {
-    struct Client {
-      @DependencyEndpoint
-      var endpoint: () -> Void
-    }
-    let client = Client()
-    XCTExpectFailure {
-      client.endpoint()
-    } issueMatcher: {
-      $0.compactDescription == """
-        Unimplemented: 'endpoint'
-        """
+  final class DependencyEndpointTests: XCTestCase {
+    func testUnimplemented() {
+      struct Client {
+        @DependencyEndpoint
+        var endpoint: () -> Void
+      }
+      let client = Client()
+      XCTExpectFailure {
+        client.endpoint()
+      } issueMatcher: {
+        $0.compactDescription == """
+          Unimplemented: 'endpoint'
+          """
+      }
     }
   }
-}
+#endif

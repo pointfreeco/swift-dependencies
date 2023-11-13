c6d1faf3d705860bd84f73cf7ad09770ab16da9e
Stephen Celis <stephen@stephencelis.com>
2023-11-13 12:03:27 -0800

    wip

--- Tests/DependenciesTests/DependencyEndpointTests.swift
+++ Tests/DependenciesTests/DependencyEndpointTests.swift
@@ -4,19 +4,21 @@
   import XCTest
 
   final class DependencyEndpointTests: XCTestCase {
-    func testUnimplemented() {
-      struct Client {
-        @DependencyEndpoint
-        var endpoint: () -> Void
+    #if os(iOS) || os(macOS) || os(tvOS) || os(watchOS)
+      func testUnimplemented() {
+        struct Client {
+          @DependencyEndpoint
+          var endpoint: () -> Void
+        }
+        let client = Client()
+        XCTExpectFailure {
+          client.endpoint()
+        } issueMatcher: {
+          $0.compactDescription == """
+            Unimplemented: 'endpoint'
+            """
+        }
       }
-      let client = Client()
-      XCTExpectFailure {
-        client.endpoint()
-      } issueMatcher: {
-        $0.compactDescription == """
-          Unimplemented: 'endpoint'
-          """
-      }
-    }
+    #endif
   }
 #endif

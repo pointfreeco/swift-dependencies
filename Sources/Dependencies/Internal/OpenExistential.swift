449b5c2761399353db02d055c93b18611071afe9
Jaesung Lee <chic0815@konkuk.ac.kr>
2023-11-19 10:44:53 +0900

    [VER] Bump up Swift compiler version to 5.7.1

--- Sources/Dependencies/Internal/OpenExistential.swift
+++ Sources/Dependencies/Internal/OpenExistential.swift
@@ -1,27 +1,3 @@
-#if swift(>=5.7)
-  // MARK: swift(>=5.7)
-
-  func _liveValue(_ key: Any.Type) -> Any? {
-    (key as? any DependencyKey.Type)?.liveValue
-  }
-#else
-  // MARK: -
-  // MARK: swift(<5.7)
-
-  private enum Witness<T> {}
-
-  func _liveValue(_ key: Any.Type) -> Any? {
-    func open<T>(_: T.Type) -> Any? {
-      (Witness<T>.self as? AnyDependencyKey.Type)?.liveValue
-    }
-    return _openExistential(key, do: open)
-  }
-
-  protocol AnyDependencyKey {
-    static var liveValue: Any { get }
-  }
-
-  extension Witness: AnyDependencyKey where T: DependencyKey {
-    static var liveValue: Any { T.liveValue }
-  }
-#endif
+func _liveValue(_ key: Any.Type) -> Any? {
+  (key as? any DependencyKey.Type)?.liveValue
+}

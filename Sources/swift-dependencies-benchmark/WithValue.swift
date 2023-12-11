5901e467d2117235e2550a1d8f324ff6d6b98374
x_0o0 <chic0815@konkuk.ac.kr>
2023-12-12 07:57:44 +0900

    [VER] Bump up Swift compiler version to 5.7.1 to follow Apple's policy (#140)
    * [VER] Bump up Swift compiler version to 5.7.1
    
    * [MOD] Modified swiftwasm version to use over 5.7.0 package

--- Sources/swift-dependencies-benchmark/WithValue.swift
+++ Sources/swift-dependencies-benchmark/WithValue.swift
@@ -3,57 +3,55 @@ import Dependencies
 import Foundation
 
 let withValueSuite = BenchmarkSuite(name: "Dependencies") { suite in
-  #if swift(>=5.7)
-    _ = DependencyValues._current.date.now
-    _ = DependencyValues._current.calendar
-    _ = DependencyValues._current.context
-    _ = DependencyValues._current.locale
-    _ = DependencyValues._current.timeZone
-    _ = DependencyValues._current.urlSession
-    _ = DependencyValues._current.uuid
-    _ = DependencyValues._current.withRandomNumberGenerator
-    @Dependency(\.someValue) var someValue: Int
+  _ = DependencyValues._current.date.now
+  _ = DependencyValues._current.calendar
+  _ = DependencyValues._current.context
+  _ = DependencyValues._current.locale
+  _ = DependencyValues._current.timeZone
+  _ = DependencyValues._current.urlSession
+  _ = DependencyValues._current.uuid
+  _ = DependencyValues._current.withRandomNumberGenerator
+  @Dependency(\.someValue) var someValue: Int
 
-    suite.benchmark("Dependency key writing") {
-      let value = withDependencies {
-        $0.someValue = 1
+  suite.benchmark("Dependency key writing") {
+    let value = withDependencies {
+      $0.someValue = 1
+    } operation: {
+      withDependencies {
+        $0.someValue = 2
       } operation: {
         withDependencies {
-          $0.someValue = 2
+          $0.someValue = 3
         } operation: {
           withDependencies {
-            $0.someValue = 3
+            $0.someValue = 4
           } operation: {
             withDependencies {
-              $0.someValue = 4
+              $0.someValue = 5
             } operation: {
               withDependencies {
-                $0.someValue = 5
+                $0.someValue = 6
               } operation: {
                 withDependencies {
-                  $0.someValue = 6
+                  $0.someValue = 7
                 } operation: {
                   withDependencies {
-                    $0.someValue = 7
+                    $0.someValue = 8
                   } operation: {
                     withDependencies {
-                      $0.someValue = 8
+                      $0.someValue = 9
                     } operation: {
                       withDependencies {
-                        $0.someValue = 9
+                        $0.someValue = 10
                       } operation: {
                         withDependencies {
-                          $0.someValue = 10
+                          $0.date = .constant(Date())
+                          $0.calendar = Calendar(identifier: .gregorian)
+                          $0.context = .live
+                          $0.locale = Locale(identifier: "en_US")
+                          $0.uuid = .incrementing
                         } operation: {
-                          withDependencies {
-                            $0.date = .constant(Date())
-                            $0.calendar = Calendar(identifier: .gregorian)
-                            $0.context = .live
-                            $0.locale = Locale(identifier: "en_US")
-                            $0.uuid = .incrementing
-                          } operation: {
-                            someValue
-                          }
+                          someValue
                         }
                       }
                     }
@@ -64,9 +62,9 @@ let withValueSuite = BenchmarkSuite(name: "Dependencies") { suite in
           }
         }
       }
-      precondition(value == 10)
     }
-  #endif
+    precondition(value == 10)
+  }
 }
 
 private enum SomeValueKey: DependencyKey {

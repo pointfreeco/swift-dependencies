b8d6f9fbb54a16190227f541a212dbd76cc4b240
Saleem Abdulrasool <compnerd@compnerd.org>
2024-01-16 11:00:57 -0800

    Dependencies: dynamically link XCTest
    Use delayed dynamic linking for registering the test observer. This is
    particularly important for Windows which does not support weak linking
    and weak symbols. Even in the case that `Dependencies` is imported due
    to use, we will not pull in the test observer until runtime, as a best
    effort. If the DependenciesTestObserver library is found in the search
    path, it will be loaded and the observer initialised. If we can't find
    the library, we will simply continue without the test observer.

---
+++ Sources/DependenciesTestObserver/TestObserver.swift
@@ -0,0 +1,22 @@
+
+import XCTest
+import XCTestDynamicOverlay
+
+#if !_runtime(_ObjC)
+  final class TestObserver: NSObject, XCTestObservation {
+    private let resetCache: @convention(c) () -> Void
+    internal init(_ resetCache: @convention(c) () -> Void) {
+      self.resetCache = resetCache
+    }
+    public func testCaseWillStart(_ testCase: XCTestCase) {
+      self.resetCache()
+    }
+  }
+#endif
+
+public func registerTestObserver(_ resetCache: @convention(c) () -> Void) {
+  guard _XCTIsTesting else { return }
+  #if !_runtime(_ObjC)
+    XCTestObservationCenter.shared.addTestObserver(TestObserver(resetCache))
+  #endif
+}

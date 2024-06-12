4b4649dc2d0e1f7e30265b0cf2b4f691d4233d59
Stephen Celis <stephen@stephencelis.com>
2024-06-12 14:18:07 -0700

    wip

--- Sources/DependenciesTestObserver/TestObserver.swift
+++ Sources/DependenciesTestObserver/TestObserver.swift
@@ -1,22 +1,21 @@
+import XCTest
 import XCTestDynamicOverlay
 
-//#if !_runtime(_ObjC) && canImport(XCTest)
-//  import XCTest
-//
-//  final class TestObserver: NSObject, XCTestObservation {
-//    private let resetCache: @convention(c) () -> Void
-//    internal init(_ resetCache: @convention(c) () -> Void) {
-//      self.resetCache = resetCache
-//    }
-//    public func testCaseWillStart(_ testCase: XCTestCase) {
-//      self.resetCache()
-//    }
-//  }
-//#endif
+#if !_runtime(_ObjC)
+  final class TestObserver: NSObject, XCTestObservation {
+    private let resetCache: @convention(c) () -> Void
+    internal init(_ resetCache: @convention(c) () -> Void) {
+      self.resetCache = resetCache
+    }
+    public func testCaseWillStart(_ testCase: XCTestCase) {
+      self.resetCache()
+    }
+  }
+#endif
 
 public func registerTestObserver(_ resetCache: @convention(c) () -> Void) {
   guard _XCTIsTesting else { return }
-//  #if !_runtime(_ObjC) && canImport(XCTest)
-//    XCTestObservationCenter.shared.addTestObserver(TestObserver(resetCache))
-//  #endif
+  #if !_runtime(_ObjC)
+    XCTestObservationCenter.shared.addTestObserver(TestObserver(resetCache))
+  #endif
 }

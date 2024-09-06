7d89478e4f0392f7d36ddf68cad55b3deae67a87
Brandon Williams <mbrandonw@hey.com>
2024-09-06 15:42:26 -0400

    clean up tests

--- Tests/DependenciesTests/SwiftTestingTests.swift
+++ Tests/DependenciesTests/SwiftTestingTests.swift
@@ -3,17 +3,26 @@
   import Testing
 
   struct SwiftTestingTests {
+    @Test(arguments: 1...5)
+    func parameterizedCachePollution(_: Int) {
+      @Dependency(Client.self) var client
+      let value = client.increment()
+      withKnownIssue(isIntermittent: true) {
+        #expect(value == 1)
+      }
+    }
+
     @Test
-    func cachePollution1() async {
-      @Dependency(\.cachedDependency) var cachedDependency: CachedDependency
-      let value = await cachedDependency.increment()
+    func cachePollution1() {
+      @Dependency(Client.self) var client
+      let value = client.increment()
       #expect(value == 1)
     }
 
     @Test
-    func cachePollution2() async {
-      @Dependency(\.cachedDependency) var cachedDependency: CachedDependency
-      let value = await cachedDependency.increment()
+    func cachePollution2() {
+      @Dependency(Client.self) var client
+      let value = client.increment()
       // NB: Wasm has different behavior here.
       #if os(WASI)
         #expect(value == 2)
@@ -22,4 +31,14 @@
       #endif
     }
   }
+
+private struct Client: TestDependencyKey {
+  var increment: @Sendable () -> Int
+  static var testValue: Client {
+    let count = LockIsolated(0)
+    return Self {
+      count.withValue { $0 += 1; return $0 }
+    }
+  }
+}
 #endif

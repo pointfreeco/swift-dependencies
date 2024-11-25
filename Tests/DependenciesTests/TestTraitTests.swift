02c7016a6039b07960acf0af22b95cf1afe00d66
Brandon Williams <mbrandonw@hey.com>
2024-11-25 11:20:08 -0600

    fix older platforms

--- Tests/DependenciesTests/TestTraitTests.swift
+++ Tests/DependenciesTests/TestTraitTests.swift
@@ -1,30 +1,32 @@
-import Dependencies
-import DependenciesTestSupport
-import Foundation
-import Testing
+#if canImport(Testing)
+  import Dependencies
+  import DependenciesTestSupport
+  import Foundation
+  import Testing
 
-@Suite(.dependency(\.uuid, .incrementing))
-struct TestTraitTests {
-  @Dependency(\.uuid) var uuid
-  
-  @Test func statefulDependency1() async throws {
-    for index in 0...100 {
-      #expect(uuid() == UUID(index))
-      try await Task.sleep(for: .milliseconds(1))
+  @Suite(.dependency(\.uuid, .incrementing))
+  struct TestTraitTests {
+    @Dependency(\.uuid) var uuid
+
+    @Test func statefulDependency1() async throws {
+      for index in 0...100 {
+        #expect(uuid() == UUID(index))
+        try await Task.sleep(for: .milliseconds(1))
+      }
+    }
+
+    @Test func statefulDependency2() async throws {
+      for index in 0...100 {
+        #expect(uuid() == UUID(index))
+        try await Task.sleep(for: .milliseconds(1))
+      }
+    }
+
+    @Test func statefulDependency3() async throws {
+      for index in 0...100 {
+        #expect(uuid() == UUID(index))
+        try await Task.sleep(for: .milliseconds(1))
+      }
     }
   }
-
-  @Test func statefulDependency2() async throws {
-    for index in 0...100 {
-      #expect(uuid() == UUID(index))
-      try await Task.sleep(for: .milliseconds(1))
-    }
-  }
-
-  @Test func statefulDependency3() async throws {
-    for index in 0...100 {
-      #expect(uuid() == UUID(index))
-      try await Task.sleep(for: .milliseconds(1))
-    }
-  }
-}
+#endif

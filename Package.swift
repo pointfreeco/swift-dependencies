ed7db250a96021d98b1a57276b8e71c5c9cf4cf1
Andrew Edwards <andrewangeta@gmail.com>
2025-01-06 16:33:16 -0500

    Include latest swift-syntax (#326)
    * Include latest swift-syntax
    
    * Set to 602
    
    * Set to 602

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
     .package(url: "https://github.com/pointfreeco/swift-clocks", from: "1.0.4"),
     .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "1.0.0"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.4.0"),
-    .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"601.0.0-prerelease"),
+    .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"602.0.0"),
   ],
   targets: [
     .target(

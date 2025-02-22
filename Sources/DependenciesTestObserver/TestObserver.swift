617cab32bafc3b2fb4cd8735c64cd29ca58a5e52
Stephen Celis <stephen@stephencelis.com>
2025-02-21 23:19:03 -0800

    wip

--- Sources/DependenciesTestObserver/TestObserver.swift
+++ Sources/DependenciesTestObserver/TestObserver.swift
@@ -1,16 +1,23 @@
 import IssueReporting
-import XCTest
+
+#if canImport(XCTest)
+  import XCTest
+#endif
 
 #if !_runtime(_ObjC)
-  final class TestObserver: NSObject, XCTestObservation {
+  final class TestObserver: NSObject {
     private let resetCache: @convention(c) () -> Void
     internal init(_ resetCache: @convention(c) () -> Void) {
       self.resetCache = resetCache
     }
-    public func testCaseWillStart(_ testCase: XCTestCase) {
-      self.resetCache()
-    }
   }
+  #if canImport(XCTest)
+    extension TestObserver: XCTestObservation {
+      public func testCaseWillStart(_ testCase: XCTestCase) {
+        self.resetCache()
+      }
+    }
+  #endif
 #endif
 
 public func registerTestObserver(_ resetCache: @convention(c) () -> Void) {

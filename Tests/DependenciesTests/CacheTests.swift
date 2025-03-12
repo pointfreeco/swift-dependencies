49d955b845e2007637823c7ba6c9db12f11420eb
John Flanagan <john.flanagan@doordash.com>
2025-03-12 08:48:57 -0500

    Prevent potential simultaneous access to cached values during cache reset

--- Tests/DependenciesTests/CacheTests.swift
+++ Tests/DependenciesTests/CacheTests.swift
@@ -13,6 +13,35 @@ final class CachedValueTests: XCTestCase {
     @Dependency(OuterDependencyTests.self) var outerDependency
     _ = outerDependency
   }
+  
+  func testDeinitCacheAccess() {
+    struct NestedDependency: TestDependencyKey {
+      static let testValue = NestedDependency()
+    }
+    
+    final class RefDependency: TestDependencyKey, Sendable {
+      deinit {
+        @Dependency(NestedDependency.self) var nested
+        _ = nested
+      }
+      
+      static var testValue: RefDependency { RefDependency() }
+    }
+
+    XCTExpectFailure {
+      @Dependency(RefDependency.self) var refDependency
+      _ = refDependency
+      
+      // Reset the cache to trigger RefDependency deinit
+      DependencyValues._current.cachedValues.resetCache()
+    } issueMatcher: { issue in
+      // Accessing a value during cache reset accesses both DependencyContextKey and the accessed
+      // dependency. Just match on the end of the error message to cover both keys.
+      issue.compactDescription.hasSuffix(
+        "Accessing a dependency during a cache reset will always return a new and uncached instance of the dependency."
+      )
+    }
+  }
 }
 
 private struct OuterDependencyTests: TestDependencyKey {

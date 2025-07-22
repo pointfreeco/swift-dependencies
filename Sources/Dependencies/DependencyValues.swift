1439a8f7655f6cb776aaf4866b52554cacccccc6
Stephen Celis <stephen@stephencelis.com>
2025-07-22 16:26:09 -0700

    Address potential preview warnings
    We currently allow folks to use `prepareDependencies` in the body of a
    preview, which works great. It does appear that previews have gotten
    fancier, especially in Xcode 26, where a preview session stays live even
    during changes to view code. These changes cause the preview body to
    re-evaluate, triggering `prepareDependencies` all over again, and
    spitting out a large purple warning into the preview console.
    
    The behavior of `prepareDependencies` is to ignore future calls and
    surface warnings in to catch misuse, but maybe we should only emit these
    warnings in the application. Preview environments are a bit treacherous
    as is, with app entry point code leaking into them, and preview trait
    code executing across multiple previews in a file. And so this branch
    proposes silencing this warning in previews entirely.
    
    Alternatives:
    
    - We could shorten the warning to a one-liner. Something like "Xcode
      Preview: 'prepareDependencies' already called this session. Ignoring."
    - We could stop showing 'prepareDependencies' as a legit tool for
      previews and instead push people towards 'withDependencies', though
      this is not great for ergonomics, especially in non-single entry point
      systems.
    
    Bonus change:
    
    This PR also updates the warning about ignoring app entry point
    dependencies in previews with instructions on how to suppress. But given
    the above, maybe we actually want to suppress this warning entirely?

--- Sources/Dependencies/DependencyValues.swift
+++ Sources/Dependencies/DependencyValues.swift
@@ -286,7 +286,38 @@ public struct DependencyValues: Sendable {
       if DependencyValues.isPreparing {
         #if canImport(SwiftUI)
           if context == .preview, Thread.isPreviewAppEntryPoint {
-            reportIssue("Ignoring dependencies prepared in preview app entry point")
+            reportIssue(
+              """
+              Xcode Previews: Ignoring dependencies prepared in app entry point.
+
+              To silence this warning, use the dependency context to conditionally prepare them:
+
+                  import Dependencies
+                  import SwiftUI
+
+                  @main
+                  struct MyApp: App {
+                +   @Dependency(\\.context) var context
+                    init() {
+                +     // Don't prepare app dependencies in previews/tests.
+                +     guard context == .live else { return }
+                      prepareDependencies {
+                        // ...
+                      }
+                      // ...
+                    }
+
+                    var body: some Scene {
+                      WindowGroup {
+                +       // Don't load root view in previews/tests.
+                +       if context == .live {
+                          // ...
+                +       }
+                      }
+                    }
+                  }
+              """
+            )
             return
           }
         #endif
@@ -295,56 +326,58 @@ public struct DependencyValues: Sendable {
         let cacheKey = CachedValues.CacheKey(id: TypeIdentifier(key), context: context)
         guard !cachedValues.cached.keys.contains(cacheKey) else {
           if cachedValues.cached[cacheKey]?.preparationID != DependencyValues.preparationID {
-            reportIssue(
-              {
-                var dependencyDescription = ""
-                if let fileID = DependencyValues.currentDependency.fileID,
-                  let line = DependencyValues.currentDependency.line
+            if context != .preview {
+              reportIssue(
                 {
-                  dependencyDescription.append(
-                    """
-                      Location:
-                        \(fileID):\(line)
+                  var dependencyDescription = ""
+                  if let fileID = DependencyValues.currentDependency.fileID,
+                     let line = DependencyValues.currentDependency.line
+                  {
+                    dependencyDescription.append(
+                      """
+                        Location:
+                          \(fileID):\(line)
 
-                    """
-                  )
-                }
-                dependencyDescription.append(
-                  Key.self == Key.Value.self
+                      """
+                    )
+                  }
+                  dependencyDescription.append(
+                    Key.self == Key.Value.self
                     ? """
-                      Dependency:
-                        \(typeName(Key.Value.self))
-                    """
+                        Dependency:
+                          \(typeName(Key.Value.self))
+                      """
                     : """
-                      Key:
-                        \(typeName(Key.self))
-                      Value:
-                        \(typeName(Key.Value.self))
-                    """
-                )
-                var argument: String {
-                  "\(function)" == "subscript(key:)"
+                        Key:
+                          \(typeName(Key.self))
+                        Value:
+                          \(typeName(Key.Value.self))
+                      """
+                  )
+                  var argument: String {
+                    "\(function)" == "subscript(key:)"
                     ? "\(typeName(Key.self)).self"
                     : "\\.\(function)"
-                }
-                return """
-                  @Dependency(\(argument)) has already been accessed or prepared.
+                  }
+                  return """
+                    @Dependency(\(argument)) has already been accessed or prepared.
 
-                  \(dependencyDescription)
+                    \(dependencyDescription)
 
-                  A global dependency can only be prepared a single time and cannot be accessed \
-                  beforehand. Prepare dependencies as early as possible in the lifecycle of your \
-                  application.
+                    A global dependency can only be prepared a single time and cannot be accessed \
+                    beforehand. Prepare dependencies as early as possible in the lifecycle of your \
+                    application.
 
-                  To temporarily override a dependency in your application, use 'withDependencies' \
-                  to do so in a well-defined scope.
-                  """
-              }(),
-              fileID: DependencyValues.currentDependency.fileID ?? fileID,
-              filePath: DependencyValues.currentDependency.filePath ?? filePath,
-              line: DependencyValues.currentDependency.line ?? line,
-              column: DependencyValues.currentDependency.column ?? column
-            )
+                    To temporarily override a dependency in your application, use \
+                    'withDependencies' to do so in a well-defined scope.
+                    """
+                }(),
+                fileID: DependencyValues.currentDependency.fileID ?? fileID,
+                filePath: DependencyValues.currentDependency.filePath ?? filePath,
+                line: DependencyValues.currentDependency.line ?? line,
+                column: DependencyValues.currentDependency.column ?? column
+              )
+            }
           } else {
             cachedValues.cached[cacheKey] = CachedValues.CachedValue(
               base: newValue,

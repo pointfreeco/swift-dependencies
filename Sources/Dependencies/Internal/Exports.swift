8456ad7a8fc314e8490fa47b209d9eb38851a0b0
Stephan Diederich <stephan.diederich@gmail.com>
2025-11-27 16:57:38 +0100

    fix WASI build
    trying to build swift-dependencies on WASI failed because of:
    
    * missing `NotificationCenter` type
    * `combine-schedulers` not building (*)
    
    exlcuding the NotificationCenter dependency on WASI builds & only depending
    on (and exporting) `CombineSchedulers` on Apple & Linux platforms fixes it.
    
    (*) I briefly looked into getting combine-schedulers to build and that
    should also be possible, though not sure if worth the effort.

--- Sources/Dependencies/Internal/Exports.swift
+++ Sources/Dependencies/Internal/Exports.swift
@@ -1,5 +1,7 @@
 @_exported import Clocks
+#if canImport(CombineSchedulers)
 @_exported import CombineSchedulers
+#endif
 @_exported import ConcurrencyExtras
 @_exported import IssueReporting
 @_exported import XCTestDynamicOverlay

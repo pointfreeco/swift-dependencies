30fc8ab37de683b71a61a33ea0586c9cbdcd5eb6
Brandon Williams <mbrandonw@hey.com>
2026-08-17 17:10:00 -0500

    Bring back preview traits.

--- Integration/Integration/IntegrationApp.swift
+++ Integration/Integration/IntegrationApp.swift
@@ -18,5 +18,36 @@ private enum IntegrationContextKey: DependencyKey {
   static let testValue = "Test"
 }
 extension DependencyValues {
-  var integrationContext: String { self[IntegrationContextKey.self] }
+  var integrationContext: String {
+    get { self[IntegrationContextKey.self] }
+    set { self[IntegrationContextKey.self] = newValue }
+  }
+}
+
+struct IntegrationContextView: View {
+  @Dependency(\.integrationContext) var integrationContext
+  var body: some View {
+    Text(self.integrationContext)
+      .font(.system(size: 100))
+  }
+}
+
+#Preview(
+  "First",
+  traits: .dependencies {
+    $0.integrationContext = "First"
+    print(#line, $0.integrationContext)
+  }
+) {
+  IntegrationContextView()
+}
+
+#Preview(
+  "Second",
+  traits: .dependencies {
+    $0.integrationContext = "Second"
+    print(#line, $0.integrationContext)
+  }
+) {
+  IntegrationContextView()
 }
--- Sources/Dependencies/Traits/PreviewTrait.swift
+++ Sources/Dependencies/Traits/PreviewTrait.swift
@@ -1,1 +1,62 @@
+#if canImport(SwiftUI) && compiler(>=6)
+  public import SwiftUI
 
+  @available(iOS 18, macOS 15, tvOS 18, watchOS 11, visionOS 2, *)
+  extension PreviewTrait where T == Preview.ViewTraits {
+    /// A trait that overrides a preview's dependencies.
+    ///
+    /// Useful for overriding several dependencies in a preview without incurring the nesting and
+    /// indentation of ``withDependencies(_:operation:)-4uz6m``.
+    ///
+    /// ```swift
+    /// #Preview(
+    ///   traits: .dependencies {
+    ///     $0.continuousClock = ImmediateClock()
+    ///     $0.date.now = Date(timeIntervalSince1970: 1234567890)
+    ///   }
+    /// ) {
+    ///   TimerView()
+    /// }
+    /// ```
+    ///
+    /// - Parameter updateValuesForPreview: A closure for updating the current dependency values
+    ///   for the lifetime of the preview.
+    public static func dependencies(
+      _ updateValuesForPreview: (inout DependencyValues) throws -> Void
+    ) -> PreviewTrait {
+      let error: (any Error)? = {
+        do {
+          DependencyValues._current.cachedValues.resetCache()
+          try prepareDependencies(updateValuesForPreview)
+          return nil
+        } catch {
+          return error
+        }
+      }()
+      return .modifier(DependenciesPreviewModifier(error: error))
+    }
+  }
+
+  private struct DependenciesPreviewModifier: PreviewModifier {
+    let error: (any Error)?
+
+    func body(content: Content, context: ()) -> some View {
+      ZStack {
+        content
+        if let error {
+          VStack {
+            Text("Preview Trait Failure")
+              .font(.headline.bold())
+            Text(error.localizedDescription)
+              .font(.subheadline)
+          }
+          .foregroundColor(Color.white)
+          .padding()
+          .background(Color.red)
+          .cornerRadius(8)
+          .opacity(0.75)
+        }
+      }
+    }
+  }
+#endif
